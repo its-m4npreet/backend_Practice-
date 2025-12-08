08c459f5148c669aada10385136a289faa806179
its-m4npreet <m4npreet006@gmail.com>
2025-12-08 18:36:42 +0530

    initializing expresss and set some path and exexute

--- day 1/index.js
+++ day 1/index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const employees=require('./employee.json');
 
 const app=express();
 app.get('/',(req,res)=>{
@@ -14,6 +15,18 @@ app.get('/contact',(req,res)=>{
 app.get('/products',(req,res)=>{
     res.send('Products page');
 });
+app.get('/employee',(req,res)=>{
+    res.send(employees);
+})
+app.get('/employee/:id',(req,res)=>{
+    const employeeId=Number(req.params.id);
+    const user=employees.find(emp=>emp.id===employeeId);
+    if(!user){
+        return res.status(404).send('Employee not found');
+    }
+    res.send(user);
+})
+const PORT=3000;
 app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
